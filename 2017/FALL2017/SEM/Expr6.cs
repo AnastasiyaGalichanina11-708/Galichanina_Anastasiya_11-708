f83032642f29ccbb8f42129f06401aed8ab242cf
AnastasiyaGalichanina <asyagalich29@gmail.com>
2017-09-21 18:39:49 +0300

    What we do on the lesson

---
+++ 2017/FALL2017/SEM/Expr6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = Convert.ToInt32(Console.ReadLine());//x1,y1 координаты точки А 
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());//x2,y2 координаты 1 точки прямой 
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            int x3 = Convert.ToInt32(Console.ReadLine());//x2,y3 координаты 2 точки прямой
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            double a = Math.Sqrt ((x2 - x1) * (x2 - x1) + (y2 - y1)*(y2 - y1));// длина отрезка от А до точки 1
+            double b = Math.Sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));// длина отрезка от 1 до точки 2
+            double c = Math.Sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));// длина отрезка от А до точки 2
+            double p = (a + b + c) / 2;  // полупериметр
+            double S = Math.Sqrt (p*(p-a)*(p-b)*(p-c));// формула Геррона
+            double h = 2 * S / b; // площадь треугольника
+            Console.WriteLine(h);
+        }
+    }
+}
---
+++ 2017/FALL2017/SEM/Expr8.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToInt32(Console.ReadLine()); //Ураванение вида y=ax+b 
+            double b = Convert.ToInt32(Console.ReadLine());
+            double c; //Ураванение вида y=cx+d
+            double d;
+            double x = Convert.ToInt32(Console.ReadLine());
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            d = y - c * x;//Пересечение отрезков происходит в одной точке
+            c = (-1) / a; // Следует из перпендикулярности прямых
+            x = (b - d) / (c - a);
+            y = a * x + b;
+            Console.WriteLine("(" + x + ";" + y + ")"); 
+        }
+    }
+}
