c2a428810e183504b087f1f297c7b5378f025473
Galichanina_Anastasiya_11-708 <31696192+AnastasiyaGalichanina11-708@users.noreply.github.com>
2019-05-28 12:10:50 +0300

    Я советую проверять этот вариант решения
    В первом решении я пыталась настроить базу данных
    Если возникнут проблемы с базой данных, можно попробовать раскоментировать строчку 
    // Database.EnsureCreated(); в конструкторе
    Data.InstagramContext

---
+++ EXAM/Exam2/Instagram/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Instagram.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace Instagram.Controllers
+{
+    public class HomeController : Controller
+    {       
+        [Authorize(Roles = "admin, user")]
+        public IActionResult Index()
+        {
+            string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
+            return Content($"ваша роль: {role}");
+            return View();
+        }
+        [Authorize(Roles = "admin")]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+    }
+}
